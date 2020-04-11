9ef488d1da55fd0a77c2e06277395d33117e26d7
reloader <37155445+reloadersystem@users.noreply.github.com>
2020-04-11 15:53:35 -0500

    en pararelo

--- app/src/main/java/com/reloader/coroutines/MainActivity.kt
+++ app/src/main/java/com/reloader/coroutines/MainActivity.kt
@@ -6,53 +6,48 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-
-   // , CoroutineScope la implementacion
-//    override val coroutineContext: CoroutineContext   // por defecto usa  Dispacher.Main
-//        get() = Dispatchers.Main + job
-//
-// poniendo  Lifecycle.scope  remplaza a esto
-//    private lateinit var job: Job
-
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-       // job = SupervisorJob() lifecycleScope lo resume por detras para cuando el activity  se destruya..
-
-        //si utilizamos  solo Job  si falla   una  las demas  corrutinas se van cancelar
-
         submit.setOnClickListener {
+            //ejemplo que tengamos  2 servicios  en pararelo
 
-            //definir  donde  ocurrirar  la courutina / en el hilo principal / o hilo secundario para eso se usa  Dispachers
+            lifecycleScope.launch(Dispatchers.Main) {
 
-            //GlobalScope  solo funciona  el tiempo de vida de este activity
-            lifecycleScope.launch(Dispatchers.Main) {   //dispachers.default  es no poner nada
-                //Main  hilo principal
-                //DispacherIO  cuando  esta esperando un resultado sea un servicio
-                //lo que hace es crear  un nuevo hilo
-                val success = withContext(Dispatchers.IO)
+                val success1 = async(Dispatchers.IO)
                 {
-                    validateLogin(username.text.toString(), password.text.toString())
-
-                    //funcion de suspension  va  bloquear un momento hasta  que haya  un resultado //Suspend
+                    validateLogin1(username.text.toString(), password.text.toString())
                 }
-                toast(if (success as Boolean) "Success" else "Failure") // aqui sale error porque  un toast solo ocurre en un hilo principal
+
+                val success2 = async(Dispatchers.IO)
+                {
+                    validateLogin2(username.text.toString(), password.text.toString())
+                }
+                toast(if (success1.await() as Boolean && success2.await() as Boolean) "Success" else "Failure") // aqui sale error porque  un toast solo ocurre en un hilo principal
+           //await  te da el resultado de esa operación
             }
 
 
         }
     }
 
-    private fun validateLogin(user: String, pass: String): Any {
+    private fun validateLogin1(user: String, pass: String): Any {
+
+        Thread.sleep(2000)
+
+        return user.isNotEmpty() && pass.isNotEmpty()
+    }
+
+    private fun validateLogin2(user: String, pass: String): Any {
 
         Thread.sleep(2000)
 
@@ -64,11 +59,5 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
-    }
-
-    override fun onDestroy() {
-
-        //job.cancel() elimina por el lifecycle
-        super.onDestroy()
     }
 }
